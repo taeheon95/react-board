1314ad4f924ed049971762c09b3d8ef3b80bcaae
taeheon95 <dalo952003@gmail.com>
2022-02-10 21:54:57 +0900

    test using hooks

--- src/components/TodoForm/TodoForm.test.tsx
+++ src/components/TodoForm/TodoForm.test.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react-hooks";
 import TodoForm from ".";
 
 interface Todo {
@@ -8,15 +9,20 @@ interface Todo {
 }
 
 test("todo", () => {
-  const [todoList, setTodoList] = useState<Todo[]>([]);
-  render(<TodoForm todoList={todoList} setTodoList={setTodoList} />);
+  const { result } = renderHook(() => useState<Todo[]>([]));
+
+  render(
+    <TodoForm todoList={result.current[0]} setTodoList={result.current[1]} />
+  );
   const inputElement = screen.getByPlaceholderText("할 일을 입력하세요.");
   expect(inputElement).toBeInTheDocument();
 });
 
 test("todo change event", () => {
-  const [todoList, setTodoList] = useState<Todo[]>([]);
-  render(<TodoForm todoList={todoList} setTodoList={setTodoList} />);
+  const { result } = renderHook(() => useState<Todo[]>([]));
+  render(
+    <TodoForm todoList={result.current[0]} setTodoList={result.current[1]} />
+  );
   const inputElement = screen.getByPlaceholderText("할 일을 입력하세요.");
   fireEvent.change(inputElement, {
     target: {
@@ -24,4 +30,21 @@ test("todo change event", () => {
     },
   });
   expect(inputElement).toHaveAttribute("value", "할 일");
+});
+
+test("todo add event", () => {
+  const { result } = renderHook(() => useState<Todo[]>([]));
+  render(
+    <TodoForm todoList={result.current[0]} setTodoList={result.current[1]} />
+  );
+  const inputElement = screen.getByPlaceholderText("할 일을 입력하세요.");
+  const buttonElemennt = screen.getByText("+");
+  fireEvent.change(inputElement, {
+    target: {
+      value: "할 일",
+    },
+  });
+  fireEvent.click(buttonElemennt);
+  expect(result.current[0].length).toBe(1);
+  expect(result.current[0][0].todo).toBe("할 일");
 });
